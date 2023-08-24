291164128299a7883954e3c09180454182574440
LIZA <114486225+liza0012@users.noreply.github.com>
2023-08-24 10:00:48 +0530

    Added solution - LeetHub

--- Flattening a Linked List - GFG/flattening-a-linked-list.cpp
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -111,32 +111,32 @@ struct Node{
 
 /*  Function which returns the  root of 
     the flattened linked list. */
-Node* mergetwolists(Node* a, Node* b){
-    Node *temp = new Node(0);
-    Node *res= temp;
-    while(a != NULL && b!=NULL){
-        if(a->data < b->data){
-            temp->bottom=a;
-            temp= temp->bottom;
-            a = a->bottom;
+Node* merge(Node *l1, Node*l2)    {
+    Node* temp=new Node(0);
+    Node* res=temp;
+    while(l1!=NULL && l2!=NULL){
+        if(l1->data<l2->data){
+            temp->bottom=l1;
+            temp=temp->bottom;
+            l1=l1->bottom;
         }else{
-            temp->bottom=b;
-            temp= temp->bottom;
-            b = b->bottom;
+            temp->bottom=l2;
+            temp=temp->bottom;;
+            l2=l2->bottom;
         }
+        
     }
-    if(a) temp->bottom=a;
-    else temp->bottom = b;
-    
+    if(l1) temp->bottom=l1;
+    else if(l2) temp->bottom=l2;
     return res->bottom;
+    
 }
 Node *flatten(Node *root)
 {
    // Your code here
-   if(root==NULL || root->next ==NULL)return root;
-   root->next= flatten(root->next);
-   root= mergetwolists(root, root->next);
+   if(root == NULL || root->next==NULL) return root;
+   root->next=flatten(root->next);
+   root=merge(root, root->next);
    return root;
-   
 }
 
