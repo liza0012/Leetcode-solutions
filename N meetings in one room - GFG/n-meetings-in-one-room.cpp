756cb3eeca14e582a7c7da2881c3d6b2f8101f57
LIZA <114486225+liza0012@users.noreply.github.com>
2023-08-25 15:19:47 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -8,32 +8,53 @@ class Solution
     public:
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
-    static bool comp(pair<int, int> a, pair<int, int> b){
-        if(a.second== b.second) return a.first<b.first;
+    bool static comp(pair<int, int> &a, pair<int, int> &b){
+        if(a.second == b.second) return a.first<b.first;
         return a.second<b.second;
     }
+    
+    // int activitySelection(vector<int> start, vector<int> end, int n)
+    // {
+    //     // Your code here
+    //     vector<pair<int, int>> p(n);
+    //     for(int i=0; i<n; i++){
+    //         p[i]= {start[i], end[i]};
+    //     }
+    //     sort(p.begin(), p.end(), comp);
+    //     int i=0, j=1;
+    //     int ans=1;
+    //   while(j<n){
+    //       if(p[i].second<p[j].first){
+    //           ans++;
+    //           i=j;
+    //       }
+    //       j++;
+           
+    //   }
+    //   return ans;
+    // }
+    
     int maxMeetings(int start[], int end[], int n)
     {
         // Your code here
-        vector<pair<int, int>> v(n);
+        // Your code here
+        vector<pair<int, int>> p(n);
         for(int i=0; i<n; i++){
-            v[i]={start[i], end[i]};
+            p[i]= {start[i], end[i]};
         }
-        sort(v.begin(), v.end(), comp);
-        int i=0;
-        int j=1;
-        int c=1;
-        while(j<n){
-            if(v[i].second<v[j].first){
-                c++;
-                i=j;
-                j++;
-            }
-            else{
-                j++;
-            }
-        }
-        return c;
+        sort(p.begin(), p.end(), comp);
+        int i=0, j=1;
+        int ans=1;
+       while(j<n){
+           if(p[i].second<p[j].first){
+               ans++;
+               i=j;
+           }
+           j++;
+           
+       }
+       return ans;
+        
     }
 };
 
