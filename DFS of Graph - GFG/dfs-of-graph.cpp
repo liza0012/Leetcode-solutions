8a53e0c54a83862bdc0195358e13df1c52362344
LIZA <114486225+liza0012@users.noreply.github.com>
2023-10-13 21:36:12 +0530

    Added solution - LeetHub

--- DFS of Graph - GFG/dfs-of-graph.cpp
+++ DFS of Graph - GFG/dfs-of-graph.cpp
@@ -5,54 +5,24 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  
     // Function to return a list containing the DFS traversal of the graph.
-    /*void dfs(int node,vector<int> &visited,vector<int> adj[],vector<int> &ans){
-        ans.push_back(node);
-        visited[node]=1;
-        for(auto it:adj[node]){
-            if(!visited[it]){
-                dfs(it,visited,adj,ans);
-            }
-        }
-    }
-
- public:
-    // Function to return a list containing the DFS traversal of the graph.
-    vector<int> dfsOfGraph(int n, vector<int> adj[]) {
-        // Code here
-        vector<int>ans;
-        vector<int>visited(n,0);
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i,visited,adj,ans);
-            }
-        }
-        return ans;
-    }*/
-    void dfs(int node,int V, vector<int> adj[], vector<int> &ans, vector<int> &vis){
+    void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls){
         vis[node]=1;
-        ans.push_back(node);
-        for(auto it: adj[node]){
+        ls.push_back(node);
+        for(auto it:adj[node]){
             if(!vis[it]){
-                dfs(it, V, adj, ans, vis);
+                dfs(it, adj, vis, ls);
             }
         }
-        
     }
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
         // Code here
-        vector<int> vis(V, 0);
+        int vis[V]={0};
         int start=0;
-        vector<int> ans;
-        for(int i=0; i<V; i++){
-            if(!vis[i]){
-                dfs(i, V, adj, ans, vis);
-            }
-            
-        }
+        vector<int> ls;
+        dfs(start, adj, vis, ls);
+        return ls;
         
-        return ans;
     }
 };
 
